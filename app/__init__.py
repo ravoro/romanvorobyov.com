eb2e81e5257d485112ff12910f5546b90ff26716
roman <ravoro@gmail.com>
2017-06-18 15:38:32 +0100

    add ability to host blog articles

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import abort, Flask, redirect, render_template, request, url_for
+from jinja2.exceptions import TemplateNotFound
+
+from .utils import slug_regex
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,8 +22,18 @@ def contact():
     return render_template('contact.html')
 
 
+@app.route("/blog/<string:article>")
+def blog_article(article: str):
+    if not slug_regex.match(article):
+        abort(400)
+    try:
+        return render_template('articles/{}.html'.format(article))
+    except TemplateNotFound:
+        abort(404)
+
+
 @app.before_request
-def trailing_slash():
+def remove_trailing_slash():
     """Redirect requests with a trailing slash to a url without trailing slash."""
     path = request.path
     if path != '/' and path.endswith('/'):
